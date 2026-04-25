51cd20edb913b7ce6d9fe78e1a8e1bc9215d98fd
Samantha <sam.j.maclaren@gmail.com>
2026-04-25 15:45:29 -0400

    feature / create base app layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'characters',
+        pathMatch: 'full'
+      },
+      {
+        path: 'characters',
+        loadComponent: () => import('./characters/characters').then(m => m.Characters)
+      }
+    ]
+  }
+];
