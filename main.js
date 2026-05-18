414537fbb4802d9fca0f37f47699a467963a8ab1
Samantha <sam.j.maclaren@gmail.com>
2026-05-18 13:44:18 -0400

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  win.loadURL('http://localhost:4200');
+}
+app.whenReady().then(createWindow);
